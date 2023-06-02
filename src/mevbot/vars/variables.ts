340b8f335bb2e223581a18e12e5bcd7ada2eadcd
Derek Zuk <derekzuk@gmail.com>
2023-06-02 11:30:56 -0700

    Renaming files to be consistent

--- src/mevbot/vars/variables.ts
+++ src/mevbot/vars/variables.ts
@@ -2,7 +2,7 @@ import { BigNumber, Contract, providers } from "ethers";
 import {
     PopulatedTransaction  } from "ethers"
 import { CARTOONS_ADDRESS, CARTOONS_ABI, CARTOONS_CONTRACT_OWNER } from '../config/cartoons-config'
-import { GWEI } from "../../util/EthGeneralUtil"
+import { GWEI } from "../../util/eth-general-util"
 export const abiDecoder = require('abi-decoder'); // https://github.com/ConsenSys/abi-decoder
 export const Web3 = require('web3')
 require('dotenv').config() // lets us use the config in the .env file
